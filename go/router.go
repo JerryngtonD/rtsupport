22c78f328c1ffc76ad16c1b1eec4c32a35fba306
jerryngton <jerryngtond@gmail.com>
2018-07-25 12:15:17 +0300

    router handle method

--- go/router.go
+++ go/router.go
@@ -1,12 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
 type Router struct {
+	rules map[string]Handler
+}
+
+func newRouter() *Router {
+	return &Router{
+		rules: make(map[string]Handler),
+	}
+}
+
+func (r *Router) Handle(msgName string, handler Handler) {
+	r.rules[msgName] = handler
 }
 
 func (e *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-
+	socket, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Print(w, err.Error())
+		return
+	}
+	client := newClient(socket)
+	client.Write()
+	client.Read()
 }
