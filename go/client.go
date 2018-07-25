c2a1b271baf03f033436d8f6d862e9cc6140c474
jerryngton <jerryngtond@gmail.com>
2018-07-25 12:23:37 +0300

    client methods : Write / Read

--- go/client.go
+++ go/client.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"math/rand"
-	"time"
+	"golang.org/x/net/websocket"
 )
 
 type Message struct {
@@ -12,52 +10,32 @@ type Message struct {
 }
 
 type Client struct {
-	send chan Message
+	send   chan Message
+	socket *websocket.Conn
 }
 
-func (client *Client) write() {
+func (client *Client) Read() {
+	var message Message
+	for {
+		if err := client.socket.ReadJSON(&message); err != nil {
+			break
+		}
+		//TODO: CALL HANDLE FUNCTION
+	}
+}
+
+func (client *Client) Write() {
 	for msg := range client.send {
-		//TODO: socket.sendJSON(msg)
-		fmt.Printf("%#v\n", msg)
+		if err := client.socket.WriteJSON(msg); err != nil {
+			break
+		}
 	}
+	client.socket.Close()
 }
 
-func (client *Client) subscribeChannels() {
-	//TODO: changefeed Query RethinkDB
-	for {
-		time.Sleep(r())
-		client.send <- Message{"channel add", ""}
-	}
-}
-
-func (client *Client) subscribeMessages() {
-	//TODO: changefeed Query RethinkDB
-	for {
-		time.Sleep(r())
-		client.send <- Message{"message add", ""}
-	}
-}
-
-func newClient() *Client {
+func newClient(socket *websocket.Conn) *Client {
 	return &Client{
-		send: make(chan Message),
+		send:   make(chan Message),
+		socket: socket,
 	}
-}
-
-func r() time.Duration {
-	return time.Millisecond * time.Duration(rand.Intn(1000))
-}
-
-func main() {
-	client := newClient()
-	go client.subscribeChannels()
-	go client.subscribeMessages()
-	client.write()
-
-	// msgChan := make(chan string)
-	// go func() {
-	// 	msgChan <- "Hello"
-	// }()
-	// msg := <-msgChan
-	// fmt.Println(msg)
 }
