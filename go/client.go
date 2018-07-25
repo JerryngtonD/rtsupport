6cd0a51a7430529ab0b155df6a32167005b42532
jerryngton <jerryngtond@gmail.com>
2018-07-25 11:12:34 +0300

    goroutines writing to chan

--- go/client.go
+++ go/client.go
@@ -1,12 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type Message struct {
+	Name string      `json:"name"`
+	Data interface{} `json:"data":`
+}
+
+type Client struct {
+	send chan Message
+}
+
+func (client *Client) write() {
+	for msg := range client.send {
+		//TODO: socket.sendJSON(msg)
+		fmt.Printf("%#v\n", msg)
+	}
+}
+
+func (client *Client) subscribeChannels() {
+	//TODO: changefeed Query RethinkDB
+	for {
+		time.Sleep(r())
+		client.send <- Message{"channel add", ""}
+	}
+}
+
+func (client *Client) subscribeMessages() {
+	//TODO: changefeed Query RethinkDB
+	for {
+		time.Sleep(r())
+		client.send <- Message{"message add", ""}
+	}
+}
+
+func newClient() *Client {
+	return &Client{
+		send: make(chan Message),
+	}
+}
+
+func r() time.Duration {
+	return time.Millisecond * time.Duration(rand.Intn(1000))
+}
 
 func main() {
-	msgChan := make(chan string)
-	go func() {
-		msgChan <- "Hello"
-	}()
-	msg := <-msgChan
-	fmt.Println(msg)
+	client := newClient()
+	go client.subscribeChannels()
+	go client.subscribeMessages()
+	client.write()
+
+	// msgChan := make(chan string)
+	// go func() {
+	// 	msgChan <- "Hello"
+	// }()
+	// msg := <-msgChan
+	// fmt.Println(msg)
 }
