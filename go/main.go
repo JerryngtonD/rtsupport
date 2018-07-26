18682db59331d433a1e5e86d3c293d686eecbd0c
jerryngton <jerryngtond@gmail.com>
2018-07-26 09:13:09 +0300

    Router can recognize types of events

--- go/main.go
+++ go/main.go
@@ -10,9 +10,9 @@ type Channel struct {
 }
 
 func main() {
-	router := &Router{}
+	router := NewRouter()
 
-	//router.Handle("channel add", addChannel)
+	router.Handle("channel add", addChannel)
 
 	http.Handle("/", router)
 	http.ListenAndServe(":5000", nil)
