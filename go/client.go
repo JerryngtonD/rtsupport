34d08a45edca383b2edf0939f663231b38cacd5b
jerryngton <jerryngtond@gmail.com>
2018-07-29 23:46:39 +0300

    Сlose handler for stopping handling messages from all channels

--- go/client.go
+++ go/client.go
@@ -47,6 +47,12 @@ func (client *Client) Write() {
 	}
 	client.socket.Close()
 }
+func (c *Client) Close() {
+	for _, ch := range c.stopChannels {
+		ch <- true
+	}
+	close(c.send)
+}
 
 func newClient(socket *websocket.Conn, findHandler FindHandler,
 	session *r.Session) *Client {
